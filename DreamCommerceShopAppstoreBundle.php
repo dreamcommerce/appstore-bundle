2c16d9b26bf95334764cacb7dbef8f79b9c66839
Przemysław Pawliczuk <przemyslaw.pawliczuk@shoper.pl>
2015-03-28 13:49:29 +0100

    cleanup

--- DreamCommerceShopAppstoreBundle.php
+++ DreamCommerceShopAppstoreBundle.php
@@ -8,16 +8,14 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class DreamCommerceShopAppstoreBundle extends Bundle
 {
+
     public function build(ContainerBuilder $container)
     {
-        //parent::build($container);
-
         $mappings = array(
             realpath(__DIR__ . '/Resources/config/doctrine/model') => 'DreamCommerce\ShopAppstoreBundle\Model'
         );
 
         $container->addCompilerPass(DoctrineOrmMappingsPass::createXmlMappingDriver($mappings));
     }
-
 
 }
