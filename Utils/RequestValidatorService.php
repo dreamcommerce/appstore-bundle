f5310b9d7274947ef6663b43ebe12304f46ebedd
Przemysław Pawliczuk <przemyslaw.pawliczuk@shoper.pl>
2015-04-01 15:05:36 +0200

    fixed scoping - CLI were unusable due requesting empty stack from console (available only for request)

--- Utils/RequestValidatorService.php
+++ Utils/RequestValidatorService.php
@@ -13,8 +13,17 @@ use Symfony\Component\HttpFoundation\RequestStack;
 
 class RequestValidatorService extends RequestValidator{
 
-    public function __construct(RequestStack $rs){
-        parent::__construct($rs->getCurrentRequest());
+    public function __construct(RequestStack $rs = null){
+        $this->setRequestStack($rs);
+    }
+
+    public function setRequestStack(RequestStack $requestStack = null){
+        if($requestStack){
+            $request = $requestStack->getCurrentRequest();
+            if($request) {
+                parent::__construct($request);
+            }
+        }
     }
 
 }
