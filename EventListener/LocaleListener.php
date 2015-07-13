0b91ebdaaaf7c8e9980c8454a073d970cd0a23c0
Przemysław Pawliczuk <przemyslaw.pawliczuk@shoper.pl>
2015-07-13 15:44:27 +0200

    locale aware support

---
+++ EventListener/LocaleListener.php
@@ -0,0 +1,40 @@
+<?PHP
+namespace DreamCommerce\ShopAppstoreBundle\EventListener;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class LocaleListener implements EventSubscriberInterface
+{
+    private $defaultLocale;
+
+    public function __construct($defaultLocale = 'en')
+    {
+        $this->defaultLocale = $defaultLocale;
+    }
+
+    public function onKernelRequest(GetResponseEvent $event)
+    {
+        $request = $event->getRequest();
+        if (!$request->hasPreviousSession()) {
+            return;
+        }
+
+        // try to see if the locale has been set as a _locale routing parameter
+        if ($locale = $request->attributes->get('_locale')) {
+            $request->getSession()->set('_locale', $locale);
+        } else {
+            $locale = $request->query->get('translations');
+            $request->setLocale($request->getSession()->get('_locale', $locale));
+        }
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return array(
+            // must be registered before the default Locale listener
+            KernelEvents::REQUEST => array(array('onKernelRequest', 17)),
+        );
+    }
+}
