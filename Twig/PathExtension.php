409b57e0d9e74b0bff7baef70024a0b4e36c52a9
Przemysław Pawliczuk <przemyslaw.pawliczuk@shoper.pl>
2015-11-17 13:35:44 +0100

    twig tags commented

--- Twig/PathExtension.php
+++ Twig/PathExtension.php
@@ -1,25 +1,32 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: eRIZ
- * Date: 2015-03-30
- * Time: 14:49
- */
-
 namespace DreamCommerce\ShopAppstoreBundle\Twig;
 
 use DreamCommerce\ShopAppstoreBundle\Utils\Url;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
+/**
+ * Class PathExtension
+ * @package DreamCommerce\ShopAppstoreBundle\Twig
+ */
 class PathExtension extends \Twig_Extension{
 
+    /**
+     * Url service
+     * @var Url
+     */
     protected $url;
 
+    /**
+     * @param Url $url
+     */
     function __construct(Url $url)
     {
         $this->url = $url;
     }
 
+    /**
+     * @inheritdoc
+     */
     public function getFunctions()
     {
 
@@ -29,11 +36,21 @@ class PathExtension extends \Twig_Extension{
 
     }
 
+    /**
+     * @see DreamCommerce\ShopAppstoreBundle\Utils\Url::generateUrl
+     * @param $name
+     * @param array $parameters
+     * @param bool|false $relative
+     * @return string
+     */
     public function getPath($name, $parameters = array(), $relative = false)
     {
         return $this->url->generateUrl($name, $parameters, $relative ? UrlGeneratorInterface::RELATIVE_PATH : UrlGeneratorInterface::ABSOLUTE_PATH);
     }
 
+    /**
+     * @inheritdoc
+     */
     public function getName(){
         return 'path_extension';
     }
