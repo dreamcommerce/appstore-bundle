9369cfca1333065841992223438bae4aea2b4047
Daniel Hornik <daniel.1302@gmail.com>
2017-03-03 10:07:55 +0100

    Added base repositories for metafields

---
+++ Repository/Shop/MetafieldValueIntRepositoryBase.php
@@ -0,0 +1,11 @@
+<?php
+namespace DreamCommerce\Bundle\ShopAppstoreBundle\Repository\Shop;
+
+
+use Doctrine\ORM\EntityRepository;
+
+
+class MetafieldValueIntRepositoryBase extends EntityRepository
+{
+
+}
